ba70cf5ad55895869a4642cf0a2fc592e601de52
MBriedis <mbriedis@printful.com>
2020-10-20 14:23:15 +0300

    Change dates

--- src/Services/ChallengeService.php
+++ src/Services/ChallengeService.php
@@ -78,8 +78,8 @@ class ChallengeService
 
         $challenge = new ChallengeModel();
         $challenge->id = 8;
-        $challenge->openFrom = Carbon::createFromDate(2020, 10, 22, 'Europe/Riga')->setTime(0, 0, 0);
-        $challenge->openUntil = Carbon::createFromDate(2020, 11, 12, 'Europe/Riga')->setTime(23, 59, 59);
+        $challenge->openFrom = Carbon::createFromDate(2020, 10, 26, 'Europe/Riga')->setTime(0, 0, 0);
+        $challenge->openUntil = Carbon::createFromDate(2020, 11, 16, 'Europe/Riga')->setTime(23, 59, 59);
         $challenge->isWalking = true;
         $all[] = $challenge;
 
