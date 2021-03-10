f12e348aa2a3c5e9ccd525de28a8c40297eb518b
MBriedis <mbriedis@printful.com>
2021-03-10 11:07:59 +0200

    Fix file size limits

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -30,6 +30,8 @@ class Bootstrap
         $this->registerErrorHandlers();
 
         ini_set('date.timezone', 'Europe/Riga');
+        ini_set('post_max_size', '30M');
+        ini_set('upload_max_filesize', '30M');
 
         $sessionPath = realpath(__DIR__ . '/../storage/sessions');
 
